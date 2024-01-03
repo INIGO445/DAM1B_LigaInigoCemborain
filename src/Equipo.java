fc10be38e0fb88137f2e3ef39be931a9cb3117a1
inigo <icemborsan@educacion.navarra.es>
2024-01-03 17:52:33 +0100

    Creacion de la clase Equipo y visualizar el ultimo metodo, es decir, intentado solucionar los problemas

---
+++ src/Equipo.java
@@ -0,0 +1,65 @@
+import java.sql.ClientInfoStatus;
+
+public class Equipo {
+    private String nombre;
+    private String ciudad;
+    private final int numMaxJugadores = 22;
+    private Jugador[] ListaJugadores;
+
+    public Equipo(String nomEquipo, String nomCiudad)
+    {
+        nombre = nomEquipo;
+        ciudad = nomCiudad;
+        ListaJugadores = new Jugador[numMaxJugadores];
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+    public int getNumJugadores()
+    {
+        int cuantos = 0;
+        for(int posicion = 0;posicion< ListaJugadores.length;posicion++)
+        {
+            if (ListaJugadores[posicion] != null)
+            {
+                cuantos ++;
+            }
+        }
+        System.out.println(cuantos);
+        return cuantos;
+    }
+    public void mostrarListaJugadores()
+    {
+        for (int posicion = 0;posicion < ListaJugadores.length;posicion++)
+        {
+            System.out.println(ListaJugadores[posicion] + ", ");
+        }
+    }
+    public void adquirirJugador(Jugador jugador)
+    {
+        for (int posicion = 0;posicion < ListaJugadores.length;posicion++)
+        {
+            if(ListaJugadores[posicion] == null)
+            {
+                ListaJugadores[posicion] = jugador;
+            }
+        }
+    }
+    public void venderJugador(String nombreJugador)
+    {
+        for (int posicion = 0;posicion < ListaJugadores.length;posicion++)
+        {
+            if (ListaJugadores[posicion] == nombreJugador)
+            {
+                ListaJugadores[posicion] = null;
+            }
+        }
+    }
+}
+
+

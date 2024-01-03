bd2a5abfc87e5eaa393fbc7425cf3fc9fef712c0
inigo <icemborsan@educacion.navarra.es>
2024-01-03 18:40:17 +0100

    Creacion de la clase Liga y finalizacion de ella

---
+++ src/Liga.java
@@ -0,0 +1,60 @@
+public class Liga {
+    private String pais;
+    private final int numMaxEqupos = 20;
+    private Equipo[] listadoEquipos;
+
+    public Liga(String ligaPais)
+    {
+        pais = ligaPais;
+        listadoEquipos = new Equipo[numMaxEqupos];
+    }
+    public void mostrarListadoEquipos()
+    {
+        for(int equipo = 0;equipo<listadoEquipos.length;equipo++)
+        {
+            System.out.println(listadoEquipos[equipo]);
+        }
+    }
+    public int getNumEquipos()
+    {
+        int numeros = 0;
+        for(int equipo = 0;equipo<listadoEquipos.length;equipo++)
+        {
+            if(listadoEquipos[equipo] != null)
+            {
+                numeros ++;
+            }
+        }
+        System.out.println(numeros);
+        return numeros;
+    }
+    public Equipo getEquipo(String nombreEquipo)
+    {
+        Equipo ElNombre = null;
+        for (int nombre = 0;nombre< listadoEquipos.length;nombre++)
+        {
+            if(listadoEquipos[nombre].getNombre() == nombreEquipo)
+            {
+                ElNombre = listadoEquipos[nombre];
+
+            }
+        }
+        System.out.println(ElNombre);
+        return ElNombre;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+    public void anadirEquipo(Equipo equipo)
+    {
+        for (int posicion = 0;posicion < listadoEquipos.length;posicion++)
+        {
+            if(listadoEquipos[posicion] == null)
+            {
+                listadoEquipos[posicion] = equipo;
+                break;
+            }
+        }
+    }
+}

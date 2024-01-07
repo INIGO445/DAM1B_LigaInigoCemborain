f23cdd266daf6b86b15690836f4b9211776ad573
inigo <icemborsan@educacion.navarra.es>
2024-01-07 19:46:05 +0100

    Actualizando el menu

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,47 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        Liga miLiga = new Liga("España");
-        System.out.println("MENU DE LA LIGA DE" + miLiga.getPais());
-        System.out.println("1-  Insertar Equipo");
-        System.out.println("2-  Insertar Jugador");
-        System.out.println("3-  Ver equipos de la liga");
-        System.out.println("4-  Ver jugadores de un equipo");
-        System.out.println("5-  Vender jugador");
-        System.out.println("6-  Salir");
-        int opcion = teclado.nextInt();
-        if(opcion == 1)
-        {
-            insertarEquipo();
+        System.out.println("Inserte el país de la liga:");
+        String pais = teclado.nextLine();
+        Liga miLiga = new Liga(pais);
+        int opcion = 1;
+        while (opcion !=6) {
+            System.out.println("MENU DE LA LIGA DE" + miLiga.getPais());
+            System.out.println("1-  Insertar Equipo");
+            System.out.println("2-  Insertar Jugador");
+            System.out.println("3-  Ver equipos de la liga");
+            System.out.println("4-  Ver jugadores de un equipo");
+            System.out.println("5-  Vender jugador");
+            System.out.println("6-  Salir");
+            opcion = teclado.nextInt();
+            if (opcion == 1)
+            {
+                insertarEquipo();
+            }
+            if (opcion == 2)
+            {
+                insertarJugador();
+            }
+            if (opcion == 3)
+            {
+                VerLiga();
+            }
+            if (opcion == 4)
+            {
+                verJugadores();
+            }
+            if (opcion == 5)
+            {
+                venderJugador();
+            }
+            if (opcion == 6)
+            {
+                break;
+            }
+            if (opcion < 1 || opcion > 6)
+            {
+                System.out.println("Error, esta opcion no esta en la lista, Volviendo al menu...");
+            }
         }
     }
     public static void insertarEquipo()
@@ -27,9 +56,29 @@ public class Main {
         System.out.println("Inserte la ciudad del equipo");
         String ciudad = equipo.nextLine();
         System.out.println("Creando equipo…");
-        System.out.println("Insertando equipo…");
         Equipo miEquipo = new Equipo(nombre, ciudad);
+        System.out.println("Insertando equipo…");
+        miLiga.anadirEquipo(miEquipo);
         System.out.println("Equipo " + nombre + " insertado");
-        if ()
+
+    }
+    public static void insertarJugador()
+    {
+        Scanner nombre = new Scanner(System.in);
+        System.out.println("Indique el nombre del equipo donde quiere insertar el jugador:");
+
+
+    }
+    public static void VerLiga()
+    {
+
+    }
+    public static void verJugadores()
+    {
+
+    }
+    public static void venderJugador()
+    {
+
     }
 }
